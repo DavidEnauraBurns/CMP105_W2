488e184347e4e79c47aa0f85e3c4f9a88045bda6
1901496 <1901496@uad.acuk>
2020-01-23 12:59:08 +0000

    Lab 2 Complete

--- Week2/CMP105App/Level.cpp
+++ Week2/CMP105App/Level.cpp
@@ -7,6 +7,17 @@ Level::Level(sf::RenderWindow* hwnd, Input* in)
 
 	// initialise game objects
 
+	if (!font.loadFromFile("font/arial.ttf"))
+	{
+		std::cout << "Error loading font\n";
+	}
+
+	text.setFont(font);
+	text.setCharacterSize(20);
+	text.setFillColor(sf::Color::Red);
+	text.setPosition(0, 0);
+
+	drag = false;
 }
 
 Level::~Level()
@@ -17,20 +28,66 @@ Level::~Level()
 // handle user input
 void Level::handleInput()
 {
+	int a = 0;
+	int b = 0;
+	int x = 0;
+	int y = 0;
+	
 
+	// keyboard input
+	if (input->isKeyDown(sf::Keyboard::W))
+	{
+		input->setKeyUp(sf::Keyboard::W);
+		std::cout << "W was pressed\n";
+	}
+	else if (input->isKeyDown(sf::Keyboard::J) && input->isKeyDown(sf::Keyboard::K) && input->isKeyDown(sf::Keyboard::L))
+	{
+		input->setKeyUp(sf::Keyboard::J);
+		input->setKeyUp(sf::Keyboard::K); 
+		input->setKeyUp(sf::Keyboard::L);
+		std::cout << "J, K and L were pressed\n";
+	}
+	else if (input->isKeyDown(sf::Keyboard::Escape))
+	{
+		window->close();
+	}
+	
+	// mouse input
+	if (input->isMouseLDown() == true)
+	{
+		if (drag == false)
+		{
+			drag = true;
+			a = input->getMouseX();
+			b = input->getMouseY();
+		}
+	}
+	if (input->isMouseLDown() == false)
+	{
+		if (drag == true)
+		{
+			drag = false;
+			x = input->getMouseX() - a;
+			y = input->getMouseY() - b;
+
+			std::cout << "Drag Distance: " << x << ", " << y;
+		}
+	}
 
 }
 
 // Update game objects
 void Level::update()
-{
-
+{	 
+	text.setString("Mouse: " + std::to_string(input->getMouseX()) + ", " + std::to_string(input->getMouseY()));
 }
 
 // Render level
 void Level::render()
 {
 	beginDraw();
+
+	window->draw(text);
 
 	endDraw();
 }
